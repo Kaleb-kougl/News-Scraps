1516a41c817862455b98fab5cda8b6d41900ecfa
Kaleb Kougl <kalebkougl@gmail.com>
2019-01-15 19:23:43 -0600

    root page

--- app/server.js
+++ app/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 // STATIC FILES
-app.use(express.static('public'));
+app.use(express.static('./views/public'));
 
 console.log(MONGODB);
 mongoose.connect(MONGODB)
@@ -48,6 +48,44 @@ app.get('/scrape', async function (req, res) {
   });
 
   res.send("Scrape Complete");
+});
+
+app.get("/articles", function (req, res) {
+  // grab all
+  db.Article.find({})
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    //fill in with note data
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function (req, res) {
+  // create new note
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      // connect with article
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
 });
 
 app.listen(PORT, () => console.log("App running on port " + PORT + "!"));
